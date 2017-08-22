2bb54e6f8a1278bf919c30410253687bee3555e7
Vincent Asante <vincenthokie@gmail.com>
2017-08-22 15:36:48 +0300

    add wtf_form definitions to handle validation

---
+++ app/forms.py
@@ -0,0 +1,26 @@
+__author__ = 'MacUser'
+
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, BooleanField, IntegerField, DateField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+
+
+class SignUpForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
+    password2 = PasswordField('password2', validators=[DataRequired()])
+
+
+class ShoppingListForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+
+
+class ShoppingListItemForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    amount = StringField('name', validators=[DataRequired()])
