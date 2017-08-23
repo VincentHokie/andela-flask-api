0763b66ff6fdae31af6dfc1cceec03554c43ecb4
Vincent Asante <vincenthokie@gmail.com>
2017-08-23 15:36:24 +0300

    common test functions updated to use client passed as a parameter

--- app/tests/common_requests.py
+++ app/tests/common_requests.py
@@ -4,45 +4,45 @@ import unittest
 class CommonRequests(unittest.TestCase):
 
     # authentication methods
-    def login(self, login_credentials):
-        return self.client().post('/auth/login', data=login_credentials)
+    def login(self, client,  login_credentials):
+        return client.post('/auth/login', data=login_credentials)
 
-    def sign_up(self, sign_up_credentials):
-        return self.client().post('/auth/register', data=sign_up_credentials)
+    def sign_up(self, client, sign_up_credentials):
+        return client.post('/auth/register', data=sign_up_credentials)
 
-    def logout(self):
-        return self.client().get('/auth/logout')
+    def logout(self, client):
+        return client.get('/auth/logout')
 
 
     # crud on a shopping list
-    def get_all_shopping_list(self):
-        return self.client().get('/shoppinglists')
+    def get_all_shopping_list(self, client):
+        return client.get('/shoppinglists')
 
-    def create_shopping_list(self, shopping_list):
-        return self.client().post('/shoppinglists', data=shopping_list)
+    def create_shopping_list(self, client,  shopping_list):
+        return client.post('/shoppinglists', data=shopping_list)
 
-    def update_shopping_list(self, shopping_list, list_id):
-        return self.client().put('/shoppinglists/'+list_id,
+    def update_shopping_list(self, client,  shopping_list, list_id):
+        return client.put('/shoppinglists/'+list_id,
                                  data=shopping_list,
                                  headers={'Content-Type': 'application/x-www-form-urlencoded'})
 
-    def delete_shopping_list(self, list_id):
-        return self.client().delete('/shoppinglists/'+list_id,
+    def delete_shopping_list(self, client, list_id):
+        return client.delete('/shoppinglists/'+list_id,
                                     headers={'Content-Type': 'application/x-www-form-urlencoded'})
 
 
     # crud on a shopping list items
-    def get_items_under_shopping_list(self, list_id):
-        return self.client().get('/shoppinglists/'+list_id)
+    def get_items_under_shopping_list(self, client, list_id):
+        return client.get('/shoppinglists/'+list_id)
 
-    def create_shopping_list_item(self, shopping_list_item, list_id):
-        return self.client().post('/shoppinglists/'+list_id+'/items', data=shopping_list_item)
+    def create_shopping_list_item(self, client, shopping_list_item, list_id):
+        return client.post('/shoppinglists/'+list_id+'/items', data=shopping_list_item)
 
-    def update_shopping_list_item(self, shopping_list_item, list_id, item_id):
-        return self.client().put('/shoppinglists/'+list_id+'/items'+item_id,
+    def update_shopping_list_item(self, client, shopping_list_item, list_id, item_id):
+        return client.put('/shoppinglists/'+list_id+'/items'+item_id,
                                  data=shopping_list_item,
                                  headers={'Content-Type': 'application/x-www-form-urlencoded'})
 
-    def delete_shopping_list_item(self, list_id, item_id):
-        return self.client().delete('/shoppinglists/'+list_id+'/items'+item_id,
+    def delete_shopping_list_item(self, client, list_id, item_id):
+        return client.delete('/shoppinglists/'+list_id+'/items'+item_id,
                                     headers={'Content-Type': 'application/x-www-form-urlencoded'})
