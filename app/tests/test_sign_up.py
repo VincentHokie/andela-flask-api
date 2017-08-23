91a6e4aa1d456790120a44f1f1ea19291db37dda
Vincent Asante <vincenthokie@gmail.com>
2017-08-23 15:33:21 +0300

    sign_up tests now functional

--- app/tests/test_sign_up.py
+++ app/tests/test_sign_up.py
@@ -1,18 +1,31 @@
 
 import unittest
 
+from flask import json
 from app.views import app, is_testing
 from app.models import db
 
 from app.models import User, ShoppingList, ShoppingListItem
 
-class SignUpTestCase(unittest.TestCase):
+from app.tests.common_requests import CommonRequests
+
+class SignUpTestCase(CommonRequests):
     """This class represents the sign up test case"""
 
     def setUp(self):
         """Define test variables and initialize app."""
-        is_testing()
         self.app = app
+
+        POSTGRES = {
+            'user': 'vince',
+            'pw': 'vince',
+            'db': 'test_db',
+            'host': 'localhost',
+            'port': '5432',
+        }
+
+        app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
         self.client = self.app.test_client
 
         # binds the app to the current context
@@ -25,11 +38,12 @@ class SignUpTestCase(unittest.TestCase):
 
         sign_up_credentials = {'username': 'vince', "email": "vincenthokie@gmail.com", "password": "123",
                                     "password2": "123"}
-        res = self.sign_up(sign_up_credentials)
 
-        count = db.session.query(User).filter(username=sign_up_credentials["username"]).count()
-        self.assertEqual(1, count)
-        self.assertEqual(res.status_code, 201)
+        with app.test_client() as client:
+            res = self.sign_up(client, sign_up_credentials)
+
+            self.assertNotEqual(User.query.filter_by(username=sign_up_credentials["username"]).first(), None)
+            self.assertEqual(res.status_code, 201)
 
 
     def test_sign_up_password_confirmation(self):
@@ -38,59 +52,68 @@ class SignUpTestCase(unittest.TestCase):
         sign_up_credentials = {'username': 'vince2', "email": "vincenthokie@gmail.com", "password": "123",
                                "password2": "1233"}
 
-        res = self.sign_up(sign_up_credentials)
-        count = db.session.query(User).filter(username=sign_up_credentials["username"]).count()
-        self.assertEqual(0, count)
-        self.assertEqual(res.status_code, 200)
-        self.assertIn("error", res.data)
+        with app.test_client() as client:
+            res = self.sign_up(client, sign_up_credentials)
+
+            self.assertEqual(User.query.filter_by(username=sign_up_credentials["username"]).first(), None)
+            self.assertEqual(res.status_code, 200)
+            self.assertIn("error", json.loads(res.data))
 
     def test_sign_up_email_required(self):
         """Test API can notice email is required (GET request)."""
 
         sign_up_credentials = {"username" : "vince3", "email": "", "password": "123",
                                "password2": "123"}
-        res = self.sign_up(sign_up_credentials)
 
-        count = db.session.query(User).filter(username=sign_up_credentials["username"]).count()
-        self.assertEqual(0, count)
-        self.assertEqual(res.status_code, 200)
-        self.assertIn("error", res.data)
+        with app.test_client() as client:
+            res = self.sign_up(client, sign_up_credentials)
+
+            self.assertEqual(User.query.filter_by(username=sign_up_credentials["username"]).first(), None)
+            self.assertEqual(res.status_code, 200)
+            self.assertIn("error", json.loads(res.data))
+
 
     def test_sign_up_password_required(self):
         """Test API can notice password is required (GET request)."""
 
         sign_up_credentials = {"username" : "vince4", "email": "vincenthokie@gmail.com", "password": "",
                                "password2": "123"}
-        res = self.sign_up(sign_up_credentials)
 
-        count = db.session.query(User).filter(username=sign_up_credentials["username"]).count()
-        self.assertEqual(0, count)
-        self.assertEqual(res.status_code, 200)
-        self.assertIn("error", res.data)
+        with app.test_client() as client:
+            res = self.sign_up(client, sign_up_credentials)
+
+            self.assertEqual(User.query.filter_by(username=sign_up_credentials["username"]).first(), None)
+            self.assertEqual(res.status_code, 200)
+            self.assertIn("error", json.loads(res.data))
+
 
     def test_sign_up_username_required(self):
         """Test API can notice username is required (GET request)."""
 
         sign_up_credentials = {"username" : "", "email": "vincenthokie@gmail.com", "password": "123",
                                "password2": "123"}
-        res = self.sign_up(sign_up_credentials)
 
-        count = db.session.query(User).filter(email=sign_up_credentials["email"]).count()
-        self.assertEqual(0, count)
-        self.assertEqual(res.status_code, 200)
-        self.assertIn("error", res.data)
+        with app.test_client() as client:
+            res = self.sign_up(client, sign_up_credentials)
+
+            self.assertEqual(User.query.filter_by(email=sign_up_credentials["email"]).first(), None)
+            self.assertEqual(res.status_code, 200)
+            self.assertIn("error", json.loads(res.data))
+
 
     def test_sign_up_password_confirm_required(self):
         """Test API can notice password is required (GET request)."""
 
         sign_up_credentials = {"username" : "vince5", "email": "vincenthokie@gmail.com", "password": "123",
                                "password2": ""}
-        res = self.sign_up(sign_up_credentials)
 
-        count = db.session.query(User).filter(username=sign_up_credentials["username"]).count()
-        self.assertEqual(0, count)
-        self.assertEqual(res.status_code, 200)
-        self.assertIn("error", res.data)
+        with app.test_client() as client:
+            res = self.sign_up(client, sign_up_credentials)
+
+            self.assertEqual(User.query.filter_by(username=sign_up_credentials["username"]).first(), None)
+            self.assertEqual(res.status_code, 200)
+            self.assertIn("error", json.loads(res.data))
+
 
     def tearDown(self):
         """teardown all initialized variables."""
