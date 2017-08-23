dfa5bd29952e983018a0f828ecb2e41bc6266167
Vincent Asante <vincenthokie@gmail.com>
2017-08-23 10:07:35 +0300

    the g object is not used, replace with session which exists between requests

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
 
 import os
 from flask import Flask
-from flask import render_template, request, jsonify, g
+from flask import render_template, request, jsonify, session, json
 from models import db, User, ShoppingListItem, ShoppingList
 from forms import LoginForm, SignUpForm, ShoppingListForm, ShoppingListItemForm
+
+import requests
 
 from flask_httpauth import HTTPBasicAuth
 auth = HTTPBasicAuth()
