aa56b5bacb2ed851f7673dcfe491f16bb1ebf58f
Unknown <vincenthokie@gmail.com>
2017-08-24 18:09:55 +0300

    add forms to process resetting of a user password

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ __author__ = 'MacUser'
 
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, BooleanField, IntegerField, DateField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo
 
 
 class LoginForm(FlaskForm):
@@ -24,3 +24,10 @@ class ShoppingListForm(FlaskForm):
 class ShoppingListItemForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
     amount = IntegerField('name', validators=[DataRequired()])
+
+class EmailForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])#
+
+class PasswordResetForm(FlaskForm):
+    password = PasswordField('password', validators=[DataRequired(), EqualTo('password_confirm')])
+    password_confirm = PasswordField('password_confirm', validators=[DataRequired(), EqualTo('password')])
