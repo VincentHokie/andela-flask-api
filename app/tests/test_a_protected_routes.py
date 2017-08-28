b7c2c0895ce7d8bcc4ac550793f12663a1e664ec
Vincent Hokie <vincenthokie@gmail.com>
2017-08-28 11:46:33 +0300

    update protected route tests to use common_requests functions

--- app/tests/test_a_protected_routes.py
+++ app/tests/test_a_protected_routes.py
@@ -10,41 +10,43 @@ class ProtectedRoutesTestCase(CommonRequests):
     def setUp(self):
         """Define test variables and initialize app."""
         self.app = app
+        self.define_db_connections(self.app)
         self.client = self.app.test_client
+        self.credentials = {"username":"", "password":""}
 
     def test_create_shopping_list(self):
         """Test API wont submit if user is not authorized (POST request)"""
 
         with app.test_client() as client:
-            res = self.create_shopping_list(client, {}, {"username":"", "password":""})
+            res = self.create_shopping_list(client, {}, self.credentials)
             self.assertEqual(res.status_code, 401)
 
     def test_retrieve_shopping_list(self):
         """Test API wont submit if user is not authorized (GET request)"""
 
         with app.test_client() as client:
-            res = client.get('/shoppinglists')
+            res = self.get_all_shopping_list(self, client, self.credentials)
             self.assertEqual(res.status_code, 401)
 
     def test_retrieve_shopping_list_items(self):
         """Test API wont retirve resources if user is not authorized (GET request)"""
 
         with app.test_client() as client:
-            res = client.get('/shoppinglists/1')
+            res = self.get_items_under_shopping_list(self, client, 1, self.credentials)
             self.assertEqual(res.status_code, 401)
 
     def test_update_shopping_list(self):
         """Test API wont submit if user is not authorized (PUT request)"""
 
         with app.test_client() as client:
-            res = client.put('/shoppinglists/1', headers={'Content-Type': 'application/x-www-form-urlencoded'})
+            res = self.update_shopping_list(client,  {}, 1, self.credentials)
             self.assertEqual(res.status_code, 401)
 
     def test_delete_shopping_list(self):
         """Test API wont submit if user is not authorized (DELETE request)"""
 
         with app.test_client() as client:
-            res = client.delete('/shoppinglists/1', headers={'Content-Type': 'application/x-www-form-urlencoded'})
+            res = self.delete_shopping_list(client, 1, self.credentials)
             self.assertEqual(res.status_code, 401)
 
 
@@ -54,19 +56,19 @@ class ProtectedRoutesTestCase(CommonRequests):
         """Test API wont submit if user is not authorized (POST request)"""
 
         with app.test_client() as client:
-            res = client.post('/shoppinglists/1/items')
+            res = self.create_shopping_list_item(client, {}, 1, self.credentials)
             self.assertEqual(res.status_code, 401)
 
     def test_update_shopping_list_item(self):
         """Test API wont submit if user is not authorized (PUT request)"""
 
         with app.test_client() as client:
-            res = client.put('/shoppinglists/1/items/1', headers={'Content-Type': 'application/x-www-form-urlencoded'})
+            res = self.update_shopping_list_item(client, {}, 1, 1, self.credentials)
             self.assertEqual(res.status_code, 401)
 
     def test_delete_shopping_list_item(self):
         """Test API wont submit if user is not authorized (DELETE request)"""
 
         with app.test_client() as client:
-            res = client.delete('/shoppinglists/1/items/1', headers={'Content-Type': 'application/x-www-form-urlencoded'})
+            res = self.delete_shopping_list_item(client, 1, 1, self.credentials)
             self.assertEqual(res.status_code, 401)
