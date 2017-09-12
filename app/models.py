7501c7dd5eb622a36f4b357083c7fde49ffae4e4
Vincent Hokie <vincenthokie@gmail.com>
2017-09-12 22:05:21 +0300

    add 'token' field in the user table to keep track of the user's token

--- app/models.py
+++ app/models.py
@@ -29,6 +29,7 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
     password = db.Column(
         db.String(255), index=True, unique=False, nullable=False)
+    token = db.Column(db.String(255), nullable=True)
 
     def __init__(self, email, username, password):
         self.email = email
