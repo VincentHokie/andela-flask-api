d66c243f1ebe80cf0f74eb5cd769b0f6279891f3
Vincent Asante <vincenthokie@gmail.com>
2017-10-11 17:02:56 +0300

    add test file that tests conditions that reqiure a signed up account

---
+++ app/tests/test_post_sign_up.py
@@ -0,0 +1,37 @@
+
+from flask import json
+from app.views import app
+from app.models import User
+
+from app.tests.common_requests import CommonRequests
+
+class PostSignUpTestCase(CommonRequests):
+    """This class represents the sign up test case"""
+
+    def setUp(self):
+        self.set_up_tests()
+        CommonRequests.set_up_user_account(self)
+
+    def test_duplicate_username(self):
+        """Test API can create a user (POST request)"""
+
+        with app.test_client() as client:
+            res = self.sign_up(client, CommonRequests.sign_up_credentials)
+            resp = json.loads(res.data)
+
+            self.assertIn("error", resp)
+            self.assertEqual(res.status_code, 200)
+
+    def test_duplicate_email(self):
+        """Test API can create a user (POST request)"""
+
+        sign_up_credentials = {
+            'username': 'vincex', "email": "vincenthokie@gmail.com",
+            "password": "123", "password2": "123"}
+
+        with app.test_client() as client:
+            res = self.sign_up(client, sign_up_credentials)
+            resp = json.loads(res.data)
+
+            self.assertIn("error", resp)
+            self.assertEqual(res.status_code, 200)
