7dead566f8760a05d42e59a9415b9ad4896494b4
Vincent Asante <vincenthokie@gmail.com>
2017-10-12 18:40:01 +0300

    refactor views to allow absolute package imports from files

--- app/v1_views.py
+++ app/v1_views.py
@@ -1,8 +1,21 @@
+import sys
+from pathlib import Path # if you haven't already done so
+file = Path(__file__).resolve()
+parent, root = file.parent, file.parents[1]
+sys.path.append(str(root))
 
+# Additionally remove the current file's directory from sys.path
+try:
+    sys.path.remove(str(parent))
+except ValueError: # Already removed
+    pass
+
+from flask import render_template, request, jsonify, session
+from app import app, auth
 
 try:
-    from .models import db, User, ShoppingListItem, ShoppingList
-    from .forms import LoginForm, SignUpForm, ShoppingListForm, \
+    from app.models import db, User, ShoppingListItem, ShoppingList
+    from app.forms import LoginForm, SignUpForm, ShoppingListForm, \
         ShoppingListItemForm, EmailForm, PasswordResetForm
 except ImportError:
     from models import db, User, ShoppingListItem, ShoppingList
