581264ee817ec00646b7221dcfab0f6e0e7669b6
Vincent Asante <vincenthokie@gmail.com>
2017-10-12 18:34:13 +0300

    add application config in the app/__init__.py file for a global effect

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,56 @@
+import os
+from . import models
+from flask import Flask
+from flask_mail import Mail
+from flask_httpauth import HTTPBasicAuth
 
-# # import a the class that will allow us initialize a flask application
-# from flask import Flask
-#
-# from .instance.config import app_config
-# from flask_sqlalchemy import SQLAlchemy
-#
-# # initialize the flask application
-# app = Flask(__name__)
-#
-# # instruct the django application to use the config file to collect settings
-# app.config.from_object('config')
-#
-# db = SQLAlchemy(app)
-#
-# # when app is initialized, get our routes and functions to process requests
-#from . import models
-#
-#
-#
-#
-# def create_app(config_name):
-#     app = Flask(__name__)
-#     app.config.from_object(app_config[config_name])
-#     app.config.from_pyfile('config.py')
-#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#     db.init_app(app)
-#
-#     return app
+auth = HTTPBasicAuth()
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config["DEBUG"] = True
+app.config["CSRF_ENABLED"] = True
+
+POSTGRES = {
+    'user': 'vince',
+    'pw': 'vince',
+    'db': 'andela-flask-api',
+    'host': 'localhost',
+    'port': '5432',
+}
+
+
+if os.environ.get("HEROKU_POSTGRESQL_CRIMSON_URL") is None:
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://%(user)s:\
+        %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = \
+        os.environ['HEROKU_POSTGRESQL_CRIMSON_URL']
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# make wft extension consider cross-site request forgery
+app.config["WTF_CSRF_ENABLED"] = False
+
+# required when the above config is enabled, this will be used to
+# generate a scrf token..should be a string that cant be easily
+# guessed in production
+app.config["SECRET_KEY"] = 'youll-never-know-what-it-is-coz-its-secret'
+
+# email server
+app.config["MAIL_SERVER"] = 'smtp.googlemail.com'
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "andelatestmail"
+app.config["MAIL_PASSWORD"] = "andelatestmail1"
+app.config["MAIL_DEFAULT_SENDER"] = "andelatestmail@gmail.com"
+
+# administrator list
+ADMINS = ['your-gmail-username@gmail.com']
+
+models.db.init_app(app)
+mail = Mail(app)
+
+from . import v1_views
