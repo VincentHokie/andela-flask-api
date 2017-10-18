7520ba770a1820e27a0fad8c1c7ead815ce32ae2
Vincent Asante <vincenthokie@gmail.com>
2017-10-18 14:16:57 +0300

    refactors configs to use environment variables

--- app/__init__.py
+++ app/__init__.py
@@ -12,20 +12,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["DEBUG"] = True
 app.config["CSRF_ENABLED"] = True
 
-POSTGRES = {
-    'user': 'vince',
-    'pw': 'vince',
-    'db': 'andela-flask-api',
-    'host': 'localhost',
-    'port': '5432',
-}
-
-
-if os.environ.get("HEROKU_POSTGRESQL_CRIMSON_URL") is None:
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://%(user)s:\
-        %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
-else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = \
+app.config["SQLALCHEMY_DATABASE_URI"] = \
         os.environ['HEROKU_POSTGRESQL_CRIMSON_URL']
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -36,16 +23,16 @@ app.config["WTF_CSRF_ENABLED"] = False
 # required when the above config is enabled, this will be used to
 # generate a scrf token..should be a string that cant be easily
 # guessed in production
-app.config["SECRET_KEY"] = 'youll-never-know-what-it-is-coz-its-secret'
+app.config["SECRET_KEY"] = os.environ['SECRET_KEY']
 
 # email server
-app.config["MAIL_SERVER"] = 'smtp.googlemail.com'
-app.config["MAIL_PORT"] = 465
-app.config["MAIL_USE_TLS"] = False
-app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = "andelatestmail"
-app.config["MAIL_PASSWORD"] = "andelatestmail1"
-app.config["MAIL_DEFAULT_SENDER"] = "andelatestmail@gmail.com"
+app.config["MAIL_SERVER"] = os.environ['MAIL_SERVER']
+app.config["MAIL_PORT"] = os.environ['MAIL_PORT']
+app.config["MAIL_USE_TLS"] = os.environ['MAIL_USE_TLS']
+app.config["MAIL_USE_SSL"] = os.environ['MAIL_USE_SSL']
+app.config["MAIL_USERNAME"] = os.environ['MAIL_USERNAME']
+app.config["MAIL_PASSWORD"] = os.environ['MAIL_PASSWORD']
+app.config["MAIL_DEFAULT_SENDER"] = os.environ['MAIL_DEFAULT_SENDER']
 
 # administrator list
 ADMINS = ['your-gmail-username@gmail.com']
