536efc1c1931fd8de02ebb791684740f4afcd1ef
Vincent Asante <vincenthokie@gmail.com>
2018-03-14 10:53:50 +0300

    switch to ubuntu image-156

--- script/pgfile.sh
+++ script/pgfile.sh
@@ -109,7 +109,7 @@ host    all             all             ::1/128                 md5
 sudo mv pg_hba.conf /etc/postgresql/9.5/main/pg_hba.conf
 sudo /etc/init.d/postgresql start
 
-sudo -u postgres psql -c `ALTER USER postgres WITH PASSWORD 'postgres';`
+echo '' | sudo -S -u postgres psql -c `ALTER USER postgres WITH PASSWORD 'postgres';`
 
 sudo /etc/init.d/postgresql stop
 sudo rm /etc/postgresql/9.5/main/pg_hba.conf
